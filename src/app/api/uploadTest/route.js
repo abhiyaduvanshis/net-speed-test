33ee6a673587fea938548ad9b0482719430bf914
Abhishek <abhishek.singh@360realtors.com>
2025-01-08 15:05:39 +0530

    Changes by abhishek

--- src/app/api/uploadTest/route.js
+++ src/app/api/uploadTest/route.js
@@ -4,24 +4,36 @@ import { NextResponse } from 'next/server';
 import fs from "node:fs/promises";
 
 export async function POST(request) {
-    const formRequestData = await request.formData()
-    const image = formRequestData.get('file')
+    try {
+        const formRequestData = await request.formData()
+        const image = formRequestData.get('file')
 
-    if(image){
-        const arrayBuffer = await image.arrayBuffer();
-        const buffer = new Uint8Array(arrayBuffer);
-        await fs.writeFile(`./public/upload/${image?.name}`, buffer);
-    }
-
-    return NextResponse.json(
-        {
-            success:true,
-            message:"done"
-        },
-        {
-            status:200
+        if(image){
+            const arrayBuffer = await image.arrayBuffer();
+            const buffer = new Uint8Array(arrayBuffer);
+            await fs.writeFile(`./public/upload/${image?.name}`, buffer);
         }
-    )
+
+        return NextResponse.json(
+            {
+                success:true,
+                message:"done"
+            },
+            {
+                status:200
+            }
+        )
+    } catch (error) {
+        return NextResponse.json(
+            {
+                success:false,
+                message:error
+            },
+            {
+                status:401
+            }
+        )
+    }
   
 
   }
