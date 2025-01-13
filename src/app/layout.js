e8329b5b21775fe83495c81abf2d40fed2c9b3d8
Abhishek <abhishek.singh@360realtors.com>
2025-01-14 00:49:59 +0530

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,9 +17,32 @@ export const metadata = {
   keywords:"speed,test,speedtest,speed test,bandwidth speed test,internet speed test,broadband speed test,speakeasy,flash,cnet,internet,network,connection,broadband,bandwidth,latency,ping,throughput,download,upload,connection,dsl,adsl,cable,t1,isp,voip,ip,ip address,tcp"
 };
 
+export const googleTag = {
+
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+  
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-VVNDYFBP3C`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VVNDYFBP3C', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
