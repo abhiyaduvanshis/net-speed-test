9316a04f87af510f623ec15b4ae1298b1eb8093e
Abhishek <abhishek.singh@360realtors.com>
2025-01-22 01:28:27 +0530

    Changes by abhishek

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header className="bg-blue-600 text-white">
             <div className="container mx-auto flex justify-between items-center py-4 px-6">
-            <h1 className="text-2xl font-bold">SpeedTest</h1>
+            <h1 className="text-2xl font-bold">GyaniMeter</h1>
             <nav>
                 <ul className="flex space-x-4">
                 <li><a href="#" className="hover:underline">Home</a></li>
