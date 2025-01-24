dce96cb2795545310fac1cfdb01d54f0d085700c
Abhishek <abhishek.singh@360realtors.com>
2025-01-25 03:35:44 +0530

    changss

--- src/app/calculator/mortgage-calculator/page.js
+++ src/app/calculator/mortgage-calculator/page.js
@@ -7,11 +7,11 @@ import MortgageCalculator from "@/_components/MortgageCalculator";
 const canonicalUrl = `https://gyanimeter.co.in/calculator/emi-calculator`;
 
 export const metadata = {
-  title: 'EMI Calculator - Calculate EMI on Home, Car & Personal Loan | Gyanimeter',
-  description: 'EMI Calculator - Calculate your Equated Monthly Installment (EMI) for home loan, car loan, and personal loan online in simple steps using EMI Calculator at Gyanimeter.',
-  keywords:'Calculate,SIP Calculator,SIP returns,Investments,EMI Calculator ',
+  title: 'Mortgage Calculator - Calculate EMI on Home, Car & Personal Loan | Gyanimeter',
+  description: 'Mortgage Calculator - Calculate your Equated Monthly Installment (EMI) for home loan, car loan, and personal loan online in simple steps using Mortgage Calculator at Gyanimeter.',
+  keywords:'Calculate,SIP Calculator,SIP returns,Investments,Mortgage Calculator ',
   openGraph: {
-    title: 'EMI Calculator',
+    title: 'Mortgage Calculator',
   },
   alternates: {
     canonical: canonicalUrl,
